a55cf44c946219d73ef2b6ec74eb76d525c73b81
이창호 <dlckdgh741@naver.com>
2026-01-17 23:30:10 +0900

    <chore> daisyUI 설정 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,4 +27,14 @@ export default {
     },
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light', 'dark'],
+    darkTheme: 'dark',
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: '',
+    logs: true,
+    themeRoot: ':root',
+  },
 } satisfies Config;
