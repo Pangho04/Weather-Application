802c26ef448bdb633b58c937f2ea4e4a8d0108bd
이창호 <dlckdgh741@naver.com>
2026-01-17 21:41:04 +0900

    <fix> 즐겨찾기 아이콘이 사이즈 Prop을 받도록 수정

--- src/shared/ui/StartIcon/StartIcon.tsx
+++ src/shared/ui/StartIcon/StartIcon.tsx
@@ -1,15 +1,20 @@
 type Props = {
   active: boolean;
+  size?: number;
 };
 
-function StartIcon({ active }: Props) {
+function StartIcon({ active, size = 15 }: Props) {
   return (
     <div className="relative flex items-center justify-center cursor-pointer active:scale-90 transition-transform">
       {/* 테두리용 별 모양 (더 큰 크기) */}
-      <div className="mask mask-star-2 size-[19px] absolute bg-gray-500" />
+      <div
+        className="mask mask-star-2 absolute bg-gray-500"
+        style={{ width: `${size + 4}px`, height: `${size + 4}px` }}
+      />
       {/* 내부 별 모양 (테두리 효과를 위한 작은 크기) */}
       <div
-        className={`mask mask-star-2 size-[15px] absolute ${active ? 'bg-orange-200' : 'bg-white'}`}
+        className={`mask mask-star-2 absolute ${active ? 'bg-orange-200' : 'bg-white'}`}
+        style={{ width: `${size}px`, height: `${size}px` }}
       />
     </div>
   );
