17323d24659cba41ac00c4dd676a837cab613be5
이창호 <dlckdgh741@naver.com>
2026-01-17 21:41:26 +0900

    <fix> 날씨 상세 페이지에서 즐겨찾기 등록/해제가 가능하도록 수정

--- src/features/location/ui/LocationHeader.tsx
+++ src/features/location/ui/LocationHeader.tsx
@@ -1,16 +1,34 @@
+import StartIcon from '@/shared/ui/StartIcon/StartIcon';
+import Box from '@/shared/ui/Box/Box';
+import { useFavoriteList } from '@/features/favorites/model';
 import { useLocationQuery } from '../model';
 
 function LocationHeader() {
   const { data: locationInfo, isLoading } = useLocationQuery();
+  const { favorites, onClickFavorite } = useFavoriteList();
 
   if (isLoading) {
     return <div className="skeleton h-12 w-1/3 lg:w-1/4" />;
   }
 
   return (
-    <p className="text-3xl lg:text-6xl font-bold pt-[12px] lg:pt-[48px]">
-      {locationInfo?.leafAddress}
-    </p>
+    <Box styleProps="flex-row items-center justify-center gap-6 lg:gap-8 w-full">
+      <p className="text-3xl lg:text-6xl font-bold pt-[12px] lg:pt-[48px]">
+        {locationInfo?.leafAddress}
+      </p>
+      <button
+        type="button"
+        className="flex items-center pt-[12px] lg:pt-[48px]"
+        onClick={() => onClickFavorite(locationInfo?.leafAddress ?? '')}
+      >
+        <StartIcon
+          active={favorites.some(
+            (favorite) => favorite.address.split('-').reverse()[0] === locationInfo?.leafAddress,
+          )}
+          size={24}
+        />
+      </button>
+    </Box>
   );
 }
 
